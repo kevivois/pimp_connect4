fb2a15c131bcb66309b7164e7f051a299c189838
fylis <filip.siliwoniuk@gmail.com>
2023-12-19 14:12:20 +0100

    creating basic methods for Game class

--- src/Game.scala
+++ src/Game.scala
@@ -1,3 +1,26 @@
+import java.awt.Color
+
 class Game {
-println("test")
+  var p1: Boolean = false
+  var p2: Boolean = false
+  var circle_list: Array[Array[Circle]] = Array.ofDim(7,6)
+
+  def changeCircleColor(p: Int,c: Circle) : Unit = {
+    if(p == 0){
+      c.color = chooseColor(p)
+    } else {
+      c.color = chooseColor(p)
+    }
+
+  }
+
+  def chooseColor(p: Int) : Color = {
+    var color: Color = new Color(0,0,0)
+    return color
+  }
+
+  def checkWin() : Unit = {
+
+  }
+
 }
