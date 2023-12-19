7147adbf336f394aacb110659a881f1f7b169ab1
kevin voisin <kevin.voisin187@gmail.com>
2023-12-19 14:11:58 +0100

    Init circle class

--- src/Circle.scala
+++ src/Circle.scala
@@ -1,6 +1,24 @@
 import java.awt.Color
-class Circle(centerX:Double,centerY:Double,radius:Double,var color:Color,var isColored:Boolean) {
-  def draw():Unit = {
-
+import  hevs.graphics.FunGraphics
+class Circle(graphics:FunGraphics,centerX:Int,centerY:Int,radius:Int,private var colored:Boolean,var color:Color=new Color(255,255,255)) {
+  private def draw():Unit = {
+    drawBorders()
+    graphics.setColor(color)
+    graphics.drawFilledCircle(centerX,centerY,radius)
   }
+  private def drawBorders():Unit = {
+    graphics.setColor(new Color(0,0,0))
+    graphics.drawCircle(centerX,centerY,radius+10)
+  }
+  def fill(clr:Color=color): Unit = {
+    colored = true
+    color = clr
+    draw()
+  }
+  def empty():Unit = {
+    color = new Color(255,255,255)
+    colored = false
+    draw()
+  }
+  def is_colored():Boolean = colored
 }
