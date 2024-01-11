330e705c37d01ed746f27da5075a1e017a44e3c0
fylis <filip.siliwoniuk@gmail.com>
2024-01-11 23:49:52 +0100

    Game.scala:   - checkWin:     * creating the horizontal check     * check for a draw     * replay method
    Grid.scala:
      - creating clearGrid function
      - creating isColumnFull function to see if a column is full (used to not increment the moveCounter with a move on a full column)

--- src/Game.scala
+++ src/Game.scala
@@ -1,24 +1,23 @@
 class Game(g: Grid) {
 
-  private var idx: Int = 0
-  private var end: Boolean = false
+  private var moveCount: Int = 1
 
   /*
-  * Main class
+  * Play method
   */
   def play(): Unit = {
-    idx = 0
     println(g.drawGrid())
     do{
-      g.showSymbol(chooseColumn())
-      idx += 1
-      checkWin()
-    } while (!end)
+      val columnChoice:Int = chooseColumn()
+      if(!g.isColumnFull(columnChoice)){
+        moveCount += 1
+      }
+      g.showSymbol(columnChoice)
+    } while (!checkWin())
   }
 
   /*
   * Gives the possibility to choose a Column using the Input Class
-  * Big thanks for ISC Team!
   */
   def chooseColumn(): Int = {
     var i: Int = 0
@@ -29,9 +28,46 @@ class Game(g: Grid) {
     i
   }
 
-  def checkWin(): Unit = {
-    if(idx == 42){
+  /*
+  * Checks after every move of a player if he/she won.
+  */
+  private def checkWin(): Boolean = {
+    var end: Boolean = false
+    val currentPlayer: String = if (moveCount % 2 == 0) "X" else "O"
+
+    // Horizontal check
+    for (i <- 0 until g.getY()) {
+      var playerCount: Int = 0
+      for (j <- 0 until g.getX()) {
+        if (g.getGrid()(i)(j) == currentPlayer) {
+          playerCount += 1
+          if (playerCount == 4) {
+            end = true
+            println(s"$currentPlayer has won!")
+            replay()
+          }
+        } else {
+          playerCount = 0
+        }
+      }
+    }
+
+    // Check for a draw
+    if (!end && moveCount == g.getX() * g.getY()) {
+      println("Nobody has won...")
+      replay()
       end = true
+    }
+    end
+  }
+
+  def replay(): Unit = {
+    g.clearGrid()
+    print("Do you want to replay ? (y/n): ")
+    if(Input.readChar().toLower == 'y'){
+      play()
+    } else {
+      println("Thanks for playing")
     }
   }
 }
--- src/Grid.scala
+++ src/Grid.scala
@@ -3,10 +3,31 @@ class Grid(x: Int, y: Int) {
   private var res: String = "\nConnect4 on console by FS and KV\n\n"
   private var grid_2d: Array[Array[String]] = Array.fill(y,x)(" ")
   private var player: Int = 0
+  private var p: String = ""
 
-  /*
-  * Draws the grid with the values from the Array grid_2d
-  */
+  def getX(): Int = return x
+  def getY(): Int = return y
+  def getGrid(): Array[Array[String]] = return grid_2d
+  def clearGrid(): Unit = {
+    for(i<- 0 until y){
+      for(j<- 0 until x){
+        grid_2d(i)(j) = " "
+      }
+    }
+    player = 0
+  }
+
+  // Checks if a column is full, the method will not permit the move counter to increment
+  def isColumnFull(column: Int): Boolean = {
+    for (i <- 0 until y) {
+      if (grid_2d(i)(column-1) == " ") {
+        return false
+      }
+    }
+    true
+  }
+
+  // Draws the grid with the values from the Array grid_2d
   def drawGrid(): String = {
     for(i<- 0 to 2*y){
       for(j<- 0 until x){
@@ -32,11 +53,8 @@ class Grid(x: Int, y: Int) {
     res
   }
 
-  /*
-  * Select the player (O or X)
-  */
-  def selectPlayer(): String = {
-    var p:String = ""
+  // Selects the player (O or X)
+  def setNextPlayer(): String = {
     if(player % 2 == 0){
       p = "X"
       player = 1
@@ -47,16 +65,14 @@ class Grid(x: Int, y: Int) {
     p
   }
 
-  /*
-  * Draw the symbol of a player on the grid
-  */
+  // Draws the symbol of a player on the grid
   def showSymbol(column: Int): Unit = {
     var doing:Boolean = true
     for (i <- y-1 to 0 by -1) {
       for (j <- 0 until x) {
         if(doing) {
           if (j == column - 1 && grid_2d(i)(column - 1) == " ") {
-            grid_2d(i)(j) = selectPlayer()
+            grid_2d(i)(j) = setNextPlayer()
             doing = false
             res = ""
             println(drawGrid())
