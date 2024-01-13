b2688882f3901b309c56b84074ada339ee8e37f4
fylis <filip.siliwoniuk@gmail.com>
2024-01-13 22:58:28 +0100

    Added functionalities - Vertical and diagonal check - clearScreen -> to separate the old game with the new game after replay - Added colors for the winner

--- src/Game.scala
+++ src/Game.scala
@@ -1,19 +1,24 @@
-class Game(g: Grid) {
+import scala.sys.process._
+
+class Game(g: Grid = new Grid()) {
 
   private var moveCount: Int = 0
+  private var numToWin: Int = 4
 
   /*
   * Play method
   */
   def play(): Unit = {
     println(g.drawGrid())
-    do{
-      val columnChoice:Int = chooseColumn()
-      if(!g.isColumnFull(columnChoice)){
+    do {
+      val columnChoice: Int = chooseColumn()
+      if (!g.isColumnFull(columnChoice)) {
         moveCount += 1
       }
       g.showSymbol(columnChoice)
-      println(moveCount)
+      if (!checkWin()) {
+        println(s"Next move : ${g.getNextPlayer()}")
+      }
     } while (!checkWin())
   }
 
@@ -23,9 +28,9 @@ class Game(g: Grid) {
   def chooseColumn(): Int = {
     var i: Int = 0
     do {
-      print("\rPlease enter a column (1-7): ")
+      print(s"\rPlease enter a column (1-${g.getX}): ")
       i = Input.readInt()
-    } while(i < 1 || i > 7)
+    } while (i < 1 || i > g.getX())
     i
   }
 
@@ -42,9 +47,9 @@ class Game(g: Grid) {
       for (j <- 0 until g.getX()) {
         if (g.getGrid()(i)(j) == currentPlayer) {
           playerCount += 1
-          if (playerCount == 4) {
+          if (playerCount == numToWin) {
             end = true
-            println(s"$currentPlayer has won!")
+            println(Console.GREEN + s"$currentPlayer has won! *Horizontal")
             replay()
           }
         } else {
@@ -54,14 +59,14 @@ class Game(g: Grid) {
     }
 
     // Vertical check
-    for (i <- 0 until g.getY()) {
+    for (i <- 0 until g.getX()) {
       var playerCount: Int = 0
-      for (j <- 0 until g.getX()) {
-        if (g.getGrid()(i)(j) == currentPlayer) {
+      for (j <- 0 until g.getY()) {
+        if (g.getGrid()(j)(i) == currentPlayer) {
           playerCount += 1
-          if (playerCount == 4) {
+          if (playerCount == numToWin) {
             end = true
-            println(s"$currentPlayer has won!")
+            println(Console.GREEN + s"$currentPlayer has won! *Vertical")
             replay()
           }
         } else {
@@ -70,23 +75,71 @@ class Game(g: Grid) {
       }
     }
 
+    // Diagonal from top-left
+    for (i <- 0 until g.getY() - numToWin + 1) {
+      var playerCount: Int = 0
+      for (j <- 0 until g.getX() - numToWin + 1) {
+        for (k <- 0 until numToWin) {
+          if (g.getGrid()(i + k)(j + k) == currentPlayer) {
+            playerCount += 1
+            if (playerCount == numToWin) {
+              end = true
+              println(Console.GREEN + s"$currentPlayer has won! *Diagonal Top-Left")
+              playerCount = 0
+              replay()
+            }
+          } else {
+            playerCount = 0
+          }
+        }
+        playerCount = 0
+      }
+    }
+
+    // Diagonal from top-right
+    for (i <- 0 until g.getY() - numToWin + 1) {
+      var playerCount: Int = 0
+      for (j <- g.getX() - 1 until g.getX() - numToWin - 1 by -1) {
+        for (k <- 0 until numToWin) {
+          if (g.getGrid()(i + k)(j - k) == currentPlayer) {
+            playerCount += 1
+            if (playerCount == numToWin) {
+              end = true
+              println(Console.GREEN + s"$currentPlayer has won! *Diagonal Top-Right")
+              replay()
+            }
+          } else {
+            playerCount = 0
+          }
+        }
+      }
+    }
+
     // Check for a draw
     if (!end && moveCount == g.getX() * g.getY()) {
+      end = true
       println("Nobody has won...")
       replay()
-      end = true
     }
     end
+  }
+
+  def clearScreen(): Unit = {
+    for (i <- 0 until 50) {
+      println()
+    }
   }
 
   def replay(): Unit = {
     g.clearGrid()
     moveCount = 0
-    print("Do you want to replay ? (y/n): ")
-    if(Input.readChar().toLower == 'y'){
+    print(Console.RESET + "Do you want to replay ? (y/n): ")
+    if (Input.readChar().toLower == 'y') {
+      clearScreen()
       play()
     } else {
       println("Thanks for playing")
+      System.exit(0)
     }
   }
 }
